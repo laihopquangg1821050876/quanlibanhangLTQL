8a2696c45192bd865f0ccf38d0e2f9e08c92370f
quanga123 <quangoc0302@gmail.com>
2021-10-26 09:36:19 +0700

    quang

---
+++ Controllers/KhachhangsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BTLLTQL.Models;
+
+namespace BTLLTQL.Controllers
+{
+    public class KhachhangsController : Controller
+    {
+        private BtlDbContext db = new BtlDbContext();
+
+        // GET: Khachhangs
+        public ActionResult Index()
+        {
+            return View(db.Khachhang.ToList());
+        }
+
+        // GET: Khachhangs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Khachhang khachhang = db.Khachhang.Find(id);
+            if (khachhang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khachhang);
+        }
+
+        // GET: Khachhangs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Khachhangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Stt,Makhachhang,Tenkhachhang,Sdt,Diachi")] Khachhang khachhang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Khachhang.Add(khachhang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(khachhang);
+        }
+
+        // GET: Khachhangs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Khachhang khachhang = db.Khachhang.Find(id);
+            if (khachhang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khachhang);
+        }
+
+        // POST: Khachhangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Stt,Makhachhang,Tenkhachhang,Sdt,Diachi")] Khachhang khachhang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(khachhang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(khachhang);
+        }
+
+        // GET: Khachhangs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Khachhang khachhang = db.Khachhang.Find(id);
+            if (khachhang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khachhang);
+        }
+
+        // POST: Khachhangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Khachhang khachhang = db.Khachhang.Find(id);
+            db.Khachhang.Remove(khachhang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
